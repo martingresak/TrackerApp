33b2a11b5e3c0e0b0a7b3d04d057676d9d10af66
martingresak <martingresak1@gmail.com>
2020-12-29 19:22:13 +0100

    chart & scroll ui

--- app/src/main/java/com/example/trackerapp/centerActivity.java
+++ app/src/main/java/com/example/trackerapp/centerActivity.java
@@ -1,47 +1,143 @@
 package com.example.trackerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.BarLineChartBase;
+import com.github.mikephil.charting.charts.CandleStickChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.data.ScatterData;
+import com.github.mikephil.charting.data.ScatterDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.color.MaterialColors;
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.slider.Slider;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class centerActivity extends AppCompatActivity {
+
+    Slider typeSlider;
+    BarChart mainBarChart;
+    LineChart mainLineChart;
+    ScatterChart mainScatterChart;
+
+    static int state;
+
+    void setState(int state) {
+        switch (state) {
+            case 0:
+                state = 0;
+                mainBarChart.setVisibility(View.VISIBLE);
+                mainBarChart.animateY(300);
+                mainLineChart.setVisibility(View.INVISIBLE);
+                mainScatterChart.setVisibility(View.INVISIBLE);
+                break;
+
+            case 1:
+                state = 1;
+                mainBarChart.setVisibility(View.INVISIBLE);
+                mainLineChart.setVisibility(View.VISIBLE);
+                mainLineChart.animateY(300);
+                mainScatterChart.setVisibility(View.INVISIBLE);
+                break;
+
+            case 2:
+                state = 2;
+                mainBarChart.setVisibility(View.INVISIBLE);
+                mainLineChart.setVisibility(View.INVISIBLE);
+                mainScatterChart.setVisibility(View.VISIBLE);
+                mainScatterChart.animateY(300);
+                break;
+
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_center);
+        ArrayList<BarLineChartBase> charts = new ArrayList<>();
 
-        BarChart mainStatChart = findViewById(R.id.mainStatChart);
-        
+        typeSlider = findViewById(R.id.typeSlider);
 
-        ArrayList<BarEntry> sleep = new ArrayList<>();
-        sleep.add(new BarEntry(0,60*6));
-        sleep.add(new BarEntry(1,60*7));
-        sleep.add(new BarEntry(2,60*8));
-        sleep.add(new BarEntry(3,60*4));
-        sleep.add(new BarEntry(4,60*6));
+        mainBarChart = findViewById(R.id.mainBarChart);
+        mainLineChart = findViewById(R.id.mainLineChart);
+        mainScatterChart = findViewById(R.id.mainScatterChart);
 
-        BarDataSet barDataSet = new BarDataSet(sleep, "Sleep");
+        charts.add(mainBarChart);
+        charts.add(mainLineChart);
+        charts.add(mainScatterChart);
+
+
+        ArrayList<Entry> sleep = new ArrayList<>();
+        sleep.add(new Entry(0,60*6));
+        sleep.add(new Entry(1,60*7));
+        sleep.add(new Entry(2,60*8));
+        sleep.add(new Entry(3,60*4));
+        sleep.add(new Entry(4,60*6));
+
+
+        typeSlider.addOnChangeListener((slider, value, fromUser) -> {
+            int v = (int) (value);
+            setState(v);
+        });
+
+        ArrayList<BarEntry> barSleep = new ArrayList<>();
+
+        for(int i = 0; i < sleep.size(); i++) {
+            float x = sleep.get(i).getX();
+            float y = sleep.get(i).getY();
+            barSleep.add(new BarEntry(x,y));
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barSleep, "");
+        LineDataSet lineDataSet = new LineDataSet(sleep, "");
+        ScatterDataSet scatterDataSet = new ScatterDataSet(sleep, "");
+
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
         barDataSet.setValueTextColor(Color.BLACK);
         barDataSet.setValueTextSize(16f);
 
+        lineDataSet.setColor(Color.BLACK);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(16f);
+
+        scatterDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        scatterDataSet.setValueTextColor(Color.BLACK);
+        scatterDataSet.setValueTextSize(16f);
+
         BarData barData = new BarData(barDataSet);
+        LineData lineData = new LineData(lineDataSet);
+        ScatterData scatterData = new ScatterData(scatterDataSet);
 
-        mainStatChart.setFitBars(true);
-        mainStatChart.setData(barData);
-        mainStatChart.getDescription().setText("Example");
-        mainStatChart.animateY(500);
+        mainScatterChart.setData(scatterData);
+        mainBarChart.setData(barData);
+        mainLineChart.setData(lineData);
 
+        mainBarChart.setFitBars(true);
+        setState(0);
     }
 }
